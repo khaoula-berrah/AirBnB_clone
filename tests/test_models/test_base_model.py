db74c2f0d46d9319efa609bdccd5f9e7106a1434
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-08-07 16:33:47 +0100

    readme file updated and test files created.

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import unittest
+from models.base_model import BaseModel
+
+"""
+    Module UnitTest of BaseModel class.
+"""
+
+
+class test_base_model(unittest.TestCase):
+    """
+        test_base_model class.
+
+        To Do a Unit test for BaseModel class.
+    """
+    
