703544c87668c30e96136bd4593afc74befa1680
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-08-07 14:18:16 +0100

    files updated.

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+    base_model module.
+
+    Module that contains, BaseModel class which all classes of inherit from.
+"""
+
+
+class BaseModel:
+    """
+        BaseModel class.
+    """
+
+    id = ""
+
+    def __init__(self):
+        """
+            BaseModel's Constructor.
+        """
+
+    def created_at(self):
+        """
+            function that assign the current time which is the creation time.
+        """
