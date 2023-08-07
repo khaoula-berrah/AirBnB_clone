d9b61887d1d060d02270d81f433d0e00eedbc177
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-08-07 15:58:07 +0100

    task 3 & 4 are created, but unittests not yet.

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""
+    Module of __init__ that initialize the whole module.
+"""
--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import uuid
+from datetime import datetime
+
 """
     base_model module.
 
@@ -14,12 +17,55 @@ class BaseModel:
 
     id = ""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             BaseModel's Constructor.
-        """
 
-    def created_at(self):
+            args (list): list of arguments, not not used.
+            kwargs (dict): dictionary of attribute names and its values.
         """
-            function that assign the current time which is the creation time.
+        if kwargs is not None and kwargs != {}:
+            for k in kwargs:
+                if k in ['created_at', 'updated_at']:
+                    self.__dict__[k] = datetime.strptime(
+                        kwargs[k], '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == '__class__':
+                    continue
+                else:
+                    self.__dict__[k] = kwargs[k]
+        else:
+            self.id = str(uuid.uuid4())
+            current_time = datetime.now()
+            self.created_at = current_time
+            self.updated_at = current_time
+
+    def __str__(self):
         """
+            function that returns a string representation of the current object.
+
+            Return:
+                (str): string representation of the current object.
+        """
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """
+            function that updates the current object, and assign the
+            update_at with the current date & time.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+            function that returns a dictionary containing all keys/values
+            of the instance.
+
+            Return:
+                (dict): dictionary containing all keys/values of
+                current instance.
+        """
+        dic = self.__dict__
+        dic["__class__"] = type(self).__name__
+        dic["created_at"] = dic["created_at"].isoformat()
+        dic["updated_at"] = dic["updated_at"].isoformat()
+        return (dic)
